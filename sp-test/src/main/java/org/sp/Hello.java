e7a1ec98328a258216395a4f9e27c309016e8be6
yangl1025 <ygl6566@163.com>
2021-04-24 02:33:47 +0800

    1

---
+++ sp-test/src/main/java/org/sp/Hello.java
@@ -0,0 +1,16 @@
+package org.sp;
+
+import org.springframework.stereotype.Repository;
+/**
+ * @ClassName: Hello
+ * @Description: bean
+ * @Author: Moore
+ * @Date: 2020-06-08 12:30
+ * @Version: V1.0
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello,ma_zhichu!");
+	}
+}
