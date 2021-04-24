1ba53e67d6e56514cd7ba798f50bda25654fb75a
yangl1025 <ygl6566@163.com>
2021-04-24 23:15:32 +0800

    1

--- sp-test/src/main/java/org/sp/service/ServiceB.java
+++ sp-test/src/main/java/org/sp/service/ServiceB.java
@@ -1,9 +1,11 @@
 package org.sp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component(value = "serviceB")
+@Lazy
 public class ServiceB {
 	@Autowired
 	private ServiceA  serviceA;
